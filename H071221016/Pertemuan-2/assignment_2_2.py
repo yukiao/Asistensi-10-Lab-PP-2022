0759f8a14c5c17c7548dd2f944dc505782ac776d
= <=>
2022-12-03 13:11:49 +0800

    Pengumpulan ulang tugas praktikum 2

---
+++ H071221016/Pertemuan-2/assignment_2_2.py
@@ -0,0 +1,34 @@
+golongan = input("Pilih golongan : ")
+daya = int(input("Masukkan daya : "))
+pemakaian = int(input("Masukkan jumlah pemakaian : "))
+
+if golongan == 'R1':
+    if daya == 900:
+        tagihan = pemakaian * 1352
+        print(f"Jumlah tagihan anda : Rp{tagihan:_}".replace('.', ',').replace('_', '.'))
+    elif daya == 1300:
+        tagihan = pemakaian * 1444.70
+        print(f"Jumlah tagihan anda : Rp{tagihan:_}".replace('.', ',').replace('_', '.'))
+    elif daya == 2200:
+        tagihan = pemakaian * 1444.70
+        print(f"Jumlah tagihan anda : Rp{tagihan:_}".replace('.', ',').replace('_', '.'))
+elif golongan == 'R2' and 3500 <= daya <= 5500 :
+    tagihan = pemakaian * 1699.53
+    print(f"Jumlah tagihan anda : Rp{tagihan:_}".replace('.', ',').replace('_', '.'))
+elif golongan == 'R3' and daya >= 6600 :
+    tagihan = pemakaian * 1699.53
+    print(f"Jumlah tagihan anda : Rp{tagihan:_}".replace('.', ',').replace('_', '.'))
+elif golongan == 'B2' and 6600 <= daya <= 200000 :
+    tagihan = pemakaian * 1444.70
+    print(f"Jumlah tagihan anda : Rp{tagihan:_}".replace('.', ',').replace('_', '.'))
+elif golongan == 'B3' and daya > 200000 :
+    tagihan = pemakaian * 1114.74
+    print(f"Jumlah tagihan anda : Rp{tagihan:_}".replace('.', ',').replace('_', '.'))
+elif golongan == 'I3' and daya >= 200000 :
+    tagihan = pemakaian * 1314.12
+    print(f"Jumlah tagihan anda : Rp{tagihan:_}".replace('.', ',').replace('_', '.'))
+elif golongan == 'P1' and 6600 <= daya <= 200000 :
+    tagihan = pemakaian * 1523.28
+    print(f"Jumlah tagihan anda : Rp{tagihan:_}".replace('.', ',').replace('_', '.'))
+else:
+    print("Data tidak valid")
